d31edcb5a4b31aa9f56c0ad94601f619fb1e3b64
Or4ng3 <imor4ng3@gmail.com>
2026-05-09 15:26:33 +0800

    feat: added test.py

---
+++ test.py
@@ -0,0 +1,35 @@
+from collections import defaultdict, Counter
+import json
+
+# from matplotlib import pyplot as plt
+import numpy as np
+import torch
+
+
+def print_encoding(model_inputs, indent=4):
+    indent_str = " " * indent
+    print("{")
+    for k, v in model_inputs.items():
+        print(indent_str + k + ":")
+        print(indent_str + indent_str + str(v))
+    print("}")
+
+
+id2label = {0: "O", 1: "B-ORD", 2: "I-ORD"}
+label2id = {"O": 0, "B-ORD": 1, "I-ORD": 2}
+
+from transformers import AutoTokenizer, AutoModelForTokenClassification
+
+tokenizer = AutoTokenizer.from_pretrained("./best_cfo_model")
+model = AutoModelForTokenClassification.from_pretrained("./best_cfo_model")
+
+input = "他是二把手"
+
+
+model_inputs = tokenizer(input, return_tensors="pt")
+with torch.no_grad():
+    outputs = model(**model_inputs)
+
+predictions = outputs.logits.argmax(dim=-1).squeeze().tolist()
+
+print(predictions)
